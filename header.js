f4885f4f545f835a9cf3f25b5d8fb9aeb0ceaf00
Lihong Li <lihongli.cs@gmail.com>
2020-12-21 19:06:43 -0800

    add header js file

---
+++ header.js
@@ -0,0 +1,22 @@
+document.write(`
+<head>
+<title>Lihong Li</title>
+<meta name="author" content="Lihong Li">
+<meta name="description" content="Personal Homepage">
+<meta name="keywords" content="Artificial Intelligence, Machine Learning, Reinforcement Learning, Contextual Bandit, Advertising, Recommendation, Dialogue Systems, Research, Rutgers, Yahoo, Microsoft, Google, Amazon">
+<meta name="classification" content="Personal Homepage">
+</head>
+
+<body bgcolor="f7f7f7" text="black" link="blue" vlink="purple" alink="red">
+
+<h1> <img src="./img/lihong-pro.jpg"> Lihong Li (<img src="./img/chinese-name.jpg" height="25">)</h1>
+
+Senior Principal Scientist <br/>
+Amazon<br/><br/>
+<a href="mailto:lihongli.cs@gmail.com">lihongli.cs@gmail.com</a> (for general academic work) <br/>
+<a href="mailto:llh@amazon.com">llh@amazon.com</a> (for Amazon related work) <br/>
+Seattle, WA, USA<br/>
+<p>
+
+<p/><hr>
+`);
